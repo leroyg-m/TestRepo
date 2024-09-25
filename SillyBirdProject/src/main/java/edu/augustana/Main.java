9b99d8f6835a91e981a0180ec2eef6807b6efe79
Leroy <leroygawu-mensah22@augustana.edu>
2024-09-25 07:07:01 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,9 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
+        Chicken chicken = new Chicken();
+
         System.out.println("Hello bird world!");
+        chicken.dance();
     }
 }
